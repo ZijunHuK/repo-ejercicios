3a9324a085169d228312e7c4426c2dff34f7e459
Zijun-Hu <zijun.hu@tiernoparla.es>
2022-09-27 19:03:14 +0200

    añado main class

---
+++ src/main/java/com/tiernoparla/ejercicioelementales/ejercicioElementales.java
@@ -0,0 +1,8 @@
+
+package com.tiernoparla.ejercicioelementales;
+
+public class ejercicioElementales {
+    public static void main(String[] args) {
+        System.out.println("hola");
+    } //main
+} //ejercicioElementales
